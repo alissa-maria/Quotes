969b7d524f9616d84c43988f78f77fc92f58dc1f
Alissa Kruiniger <alissa.kruiniger@gmail.com>
2023-12-11 09:47:16 +0100

    Added stuff + server config files

--- app/resources/quotes/QuotesController.jl
+++ app/resources/quotes/QuotesController.jl
@@ -9,7 +9,17 @@ function form()
   html(:quotes, :form, context = @__MODULE__)
 end
 
-function create()
+function save_quote(quote_, success_message, error_message)
+  if save(quote_)
+    flash(success_message)
+    redirect(:home)
+  else
+    flash(error_message)
+    throw(ExceptionalResponse(redirect(:home)))
+  end
+end
+
+function create_or_update(id = nothing)
   authenticated!()
 
   san_type::String = sanitize(postpayload(:type))
@@ -18,20 +28,51 @@ function create()
   san_context::String = sanitize(postpayload(:context))
   san_date::String = sanitize(postpayload(:date))
 
-  submitted = Quote(type=san_type, quote_=san_quote, name=san_from, context=san_context, date=san_date) |> save
-
-  if submitted
-    flash("Citaat geplaatst! ヽ(⌒▽⌒)ﾉ ")
-    redirect(:home)
+  if isnothing(id)
+    quote_ = Quote(type=san_type, quote_=san_quote, name=san_from, context=san_context, date=san_date)
+    save_quote(quote_, "Citaat geplaatst! ヽ(⌒▽⌒)ﾉ ", "Er is iets misgegaan, het citaat is niet geplaatst. ")
   else
-    flash("Er is iets misgegaan, het citaat is niet geplaatst. ")
-    throw(ExceptionalResponse(redirect(:home)))
+    quote_ = get(Quote, id)
+    quote_.type = san_type
+    quote_.quote_ = san_quote
+    quote_.name = san_from
+    quote_.context = san_context
+    quote_.date = san_date
+    save_quote(quote_, "Citaat bijgewerkt! ヽ(⌒▽⌒)ﾉ ", "Er is iets misgegaan, het citaat is niet bijgewerkt. ")
   end
+end
+
+function create()
+  create_or_update()
+end
+
+function update(id)
+  create_or_update(id)
 end
 
 function index()
   authenticated!()
-  html(:quotes, :index, quotes = all(Quote, order=SQLOrder(:date, "DESC")), context = @__MODULE__)
+  quotes = all(Quote, order=SQLOrder(:date, "DESC"))
+  html(:quotes, :index, quotes = quotes, context = @__MODULE__)
+end
+
+function edit(id)
+  authenticated!()
+  quote_ = get(Quote, id)
+  html(:quotes, :edit, quote_ = quote_, context = @__MODULE__)
+end
+
+function delete(id)
+  authenticated!()
+  quote_ = get(Quote, id)
+
+  if delete!(quote_)
+    flash("Citaat verwijderd! (｡♥‿♥｡) ")
+    redirect(:home)
+  else
+    flash("Er is iets misgegaan, het citaat is niet verwijderd. ")
+    throw(ExceptionalResponse(redirect(:home)))
+  end
 end
 
 function sanitize(text::String)
--- routes.jl
+++ routes.jl
@@ -5,3 +5,7 @@ route("/", QuotesController.form, named=:home)
 route("/create_quote", QuotesController.create, method=POST, named=:create_quote)
 
 route("/index", QuotesController.index, named=:index)
+
+route("/edit/:id") do
+    QuotesController.edit(:id)
+end
